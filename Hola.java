456334729efa15f4f864178750d58210cc621edd
santi <santiagofuentesespinosa@gmail.com>
2024-04-25 16:04:22 +0200

    otra version 1.0

---
+++ Hola.java
@@ -0,0 +1,10 @@
+public class Hola {
+
+    public static void main(String[] args) {
+
+        String a = "Santi";
+        String b = "Maria";
+        System.out.println(a +" y "+b);
+    
+    }
+}
